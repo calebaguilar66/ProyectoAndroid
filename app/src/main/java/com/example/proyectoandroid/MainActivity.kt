3ba53d6b65784a5eb7866e591d5ac91de2ac79ea
Caleb Aguilar <c.aguilar66@alumnos.santotomas.cl>
2024-09-30 20:09:34 -0300

    Diseño de Login sin boton de logeo

--- app/src/main/java/com/example/proyectoandroid/MainActivity.kt
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.proyectoandroid.ui.theme.ProyectoAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProyectoAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Login(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -32,24 +32,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Login(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logo), null,
+            modifier = Modifier.size(250.dp)
+        )
+
+        Column(
+            modifier = modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("¡Hola!", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Text("Inicia sesión y descubre")
+            Text("todo lo nuevo que tenemos para ti!")
+        }
+        Column(
+            modifier = modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField(value="", onValueChange = {}, label = {
+                Text("Ingrese correo electrónico")
+            })
+            Spacer(modifier = Modifier.height(7.dp))
+
+
+            OutlinedTextField(value="", onValueChange = {}, label = {
+                Text("Ingrese contraseña")
+            })
+            Spacer(modifier = Modifier.height(7.dp))
+        }
+        Column(
+            modifier = modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginPreview() {
     ProyectoAndroidTheme {
-        Column {
-            Greeting("Android")
-            Prueba()
-        }
+        Login()
     }
-}
-@Composable
-fun Prueba(){
-    Text("Hola")
 }
