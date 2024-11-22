eae695f3366b7c0e6d598294792572bb52087e05
calebaguilar66 <c.aguilar66@alumnos.santotomas.cl>
2024-11-22 04:43:29 -0300

    MainActivity Modificado inicializando la base de datos sqlite y agregando nueva ruta de FormularioHorario

--- app/src/main/java/com/example/proyectoandroid/MainActivity.kt
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.proyectoandroid
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,11 +9,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.proyectoandroid.ui.theme.ProyectoAndroidTheme
 import com.example.proyectoandroid.ui.composables.*
+import com.example.proyectoandroid.data.HorarioViewModel
 
 
 class MainActivity : ComponentActivity() {
@@ -30,14 +33,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PantallaPrincipal() {
+
     val navController = rememberNavController()
+    val context = LocalContext.current.applicationContext as Application
+    val horarioViewModel = remember { HorarioViewModel(context) }
+
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         NavHost(navController = navController, startDestination = "login") {
             composable("login") { Login(modifier = Modifier.padding(innerPadding), navController = navController) }
             composable("protocolos") { Protocolos(modifier = Modifier.padding(innerPadding), navController = navController) }
             composable("gps") { GPSDireccion(modifier = Modifier.padding(innerPadding), navController = navController) }
             composable("noticias") { Noticias(modifier = Modifier.padding(innerPadding), navController = navController) }
-            composable("horario") { Horarios(modifier = Modifier.padding(innerPadding), navController = navController) }
+            composable("horario") { Horarios(modifier = Modifier.padding(innerPadding), navController = navController, viewModel = horarioViewModel) }
+            composable("formularioHorarios"){ FormularioHorarios(navController = navController, viewModel = horarioViewModel) }
         }
     }
 }
